7c10605cca5b360ff173fbd273fb3f0906b657bd
Davis Maloch (Fl. Memory) <133890108+admaloch-dos@users.noreply.github.com>
2024-03-13 09:46:49 -0400

    Add files via upload

--- merge-word-docs/merge-word-docs.bat
+++ merge-word-docs/merge-word-docs.bat
@@ -1,18 +1,45 @@
 @echo off
 setlocal enabledelayedexpansion
 
-::location of direc (make sure DocxMerge.exe is located here)
-cd /d C:\Users\%USERNAME%\Desktop\react-intro-notes
+::Intro text
 
+echo.
+echo    ********************************************************
+echo    ********* Florida Memory Combine Word Documents ********
+echo    ********************************************************
+echo.
+
+echo.
+echo Instructions:
+echo.
+echo 1. Locate the file called Add Word Documents.
+echo 2. Drag and drop the files you want to combine.
+echo 3. Add a name for the new file below.
+echo 4. Click enter when you're ready to create the new file.
+echo.
+
+::determine current location
+@REM set "main_location=C:\Users\%USERNAME%\Desktop\Merge Word Documents"
+set "main_location=%~dp0"
+::set word docs file
+cd /d %main_location%\add-word-docs-here
+::  DocxMerge.exe file
+set "merge_tool=%main_location%\DocxMerge.exe"
+:: Set the output location
+set "output_location=C:\Users\%USERNAME%\Desktop"
 ::file type
-set "file_type=.txt"
+set "file_type=.docx"
 
 set "files="
 
 for %%i in (*%file_type%) do set files=!files! "%%i"
 
-::output location
-set "name=react-intro-notes"
+::prompt user for file name
+set /p user_name_input="Enter name: "
 
-::run with docMerge
-DocxMerge.exe -i %files% -o "%name%.docx" -f
+:: set the file name with timestamp
+set TIMESTAMP=%DATE:/=-%_%TIME::=-%
+set TIMESTAMP=%TIMESTAMP: =%
+set "file_name=%user_name_input%_%TIMESTAMP%"
+
+"%merge_tool%" -i %files% -o "%output_location%\%file_name%.docx" -f
